9470186e626383bbfb7fbd0bd990a539d80beb3d
kachaMukabe <kachamukabe@gmail.com>
2021-11-18 08:52:30 +0200

    heroku deployment

--- app.py
+++ app.py
@@ -262,6 +262,6 @@ def get_transcription():
 
     return render_template_string(templ, transciption=transcript_text, podcast=episode)
 
-if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+# if __name__ == "__main__":
+    # db.create_all()
+    # app.run(debug=True)
